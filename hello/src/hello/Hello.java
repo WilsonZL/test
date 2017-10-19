f1449abfd3155367dcabfbef1f5e742cb5b1cff3
WilsonZL <57145875@qq.com>
2017-10-19 20:29:36 +0800

    commit Hello.

---
+++ hello/src/hello/Hello.java
@@ -0,0 +1,10 @@
+package hello;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		System.out.println("你好，我开始学OOP了");
+	}
+}
